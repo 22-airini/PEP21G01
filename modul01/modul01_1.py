dc09e92703cfd714b85123111813345011489143
kope_22 <airini.michel@yahoo.com>
2021-02-17 21:35:13 +0200

    modul 1

---
+++ modul01/modul01_1.py
@@ -0,0 +1,26 @@
+print('print\u200b("")')
+
+
+print(print)
+print(3)
+print('My name is:', ' Airini Michel', '1', sep='_', end='*')
+print('My name is:', ' Airini Michel', '2', sep='_', end='#')
+print('My name is:', ' Airini Michel', '3', sep='_', end='@')
+
+print()
+print_text1= 'Hello World 1'
+print_text2= 'Hello World 2'
+print(print_text1, print_text2)
+
+print_text1= 123
+print_text2= 456
+print(print_text1, print_text2)
+
+
+#print shape
+print("***")
+print("**")
+print("*")
+print("[", "]")
+
+
